c7e496cb1942ba3f04346e5d2a1a4e05c006a8eb
Alisson Patrick <alisson@vortice.inf.br>
2019-04-10 14:32:37 -0300

    até aula 81

---
+++ Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/SearchBarPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SearchBarPage : ContentPage
+	{
+        private List<string> empresaTi;
+		public SearchBarPage ()
+		{
+			InitializeComponent ();
+
+            empresaTi = new List<string>();
+            empresaTi.Add("Microsoft");
+            empresaTi.Add("Apple");
+            empresaTi.Add("Oracle");
+            empresaTi.Add("IBM");
+            empresaTi.Add("SAP");
+            empresaTi.Add("Uber");
+            empresaTi.Add("99Taxi");
+
+            PreencherLista(empresaTi);
+		}
+
+        //private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    var resultado = empresaTi.Where(a => a.Contains(e.NewTextValue)).ToList<string>();
+        //    PreencherLista(resultado);
+        //}
+
+        private void PreencherLista(List<string> empresaTi)
+        {
+            ListResult.Children.Clear();
+
+            foreach (var item in empresaTi)
+            {
+                ListResult.Children.Add(new Label { Text = item });
+            }
+        }
+
+        private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            var resultado = empresaTi.Where(a => a.Contains(((SearchBar)sender).Text)).ToList<string>();
+            PreencherLista(resultado);
+        }
+    }
+}
---
+++ Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+    {
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoActivityIndicatorPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.ActivityIndicatorPage();
+        }
+
+        private void GoProgressBarPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.ProgressBarPage();
+        }
+
+        private void GoBoxViewPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.BoxViewPage();
+        }
+
+        private void GoLabelPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.LabelPage();
+            IsPresented = false;
+        }
+
+        private void GoButtonPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.ButtonPage();
+            IsPresented = false;
+        }
+
+        private void GoEntryEditorPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.EntryEditorPage();
+            IsPresented = false;
+        }
+
+        private void GoDatePickerPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.DatePickerPage();
+            IsPresented = false;
+        }
+
+        private void GoTimerPickerPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.TimerPickerPage();
+            IsPresented = false;
+        }
+        private void GoPickerPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.PickerPage();
+            IsPresented = false;
+        }
+
+        private void GoSearchPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.SearchBarPage();
+            IsPresented = false;
+        }
+
+        private void GoSliderStepperPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.SliderStepperPage();
+            IsPresented = false;
+        }
+
+        private void GoSwitchPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.SwitchPage();
+            IsPresented = false;
+        }
+
+        private void GoImagePage(object sender, EventArgs e)
+        {
+            Detail = new Controles.ImagePage();
+            IsPresented = false;
+        }
+
+        private void GoListViewPage(object sender, EventArgs e)
+        {
+            Detail = new Controles.ListViewPage();
+            IsPresented = false;
+        }
+    }
+}
